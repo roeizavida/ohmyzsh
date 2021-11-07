a2aeb31ab9fbd463fcfd83b54f3c4b4ae0097a74
Roei Zavida <roeizavida@users.noreply.github.com>
2021-11-07 10:04:07 +0200

    Add venv plugin

---
+++ custom/plugins/venv/venv.plugin.zsh
@@ -0,0 +1,19 @@
+# Auto activate and deactivate Python venv.
+# Only works for venv dirs with the name ".venv" in the working directory.
+
+function chpwd() {
+  # Check if ".venv" dir exists in the working directory
+  if [[ -d ".venv" ]]
+  then
+    # Activate venv
+    source ./.venv/bin/activate
+  # If ".venv" dir does not exists
+  else
+    # Check if venv is activated (".venv" in Python path)
+    if [[ $(which python) == *"/.venv/bin/python" ]]
+    then
+      # Deactivate venv
+      deactivate
+    fi
+  fi
+}
