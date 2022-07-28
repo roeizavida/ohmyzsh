38889358fd18d03806807ab1ffd79d3ce6d979f0
Roei Zavida <roeizavida@users.noreply.github.com>
2022-07-28 16:52:28 +0300

    update conf

--- custom/plugins/roei-configs/roei-configs.plugin.zsh
+++ custom/plugins/roei-configs/roei-configs.plugin.zsh
@@ -5,8 +5,8 @@
 git_configs_path=$(find ~/git -type d -name 'configs')
 
 # Set config aliases
-alias sync-conf="$git_configs_path/scripts/sync_config.sh"
-alias fetch-conf="$git_configs_path/scripts/fetch_config.sh"
+alias sync-conf="python3 $git_configs_path/scripts/config.py sync"
+alias fetch-conf="python3 $git_configs_path/scripts/config.py fetch"
 
 # Load Aliases
 source ~/.oh-my-zsh/roei/aliases.zsh
