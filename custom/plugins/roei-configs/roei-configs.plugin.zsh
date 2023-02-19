6ce4654c97380c1c4612f9532c112407a3e4b4f2
Roei Zavida <roeizavida@users.noreply.github.com>
2023-02-19 09:41:22 +0200

    fix git_configs_path

--- custom/plugins/roei-configs/roei-configs.plugin.zsh
+++ custom/plugins/roei-configs/roei-configs.plugin.zsh
@@ -2,7 +2,7 @@
 # Configs
 ###############################################
 # Get git configs repo path
-git_configs_path=$(find ~/git -type d -name 'configs')
+git_configs_path=~/git/configs
 
 # Set config aliases
 alias sync-conf="python3 $git_configs_path/scripts/config.py sync"
