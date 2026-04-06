65476836811d53006eba6c33a79c4df31241abbc
Roei Zavida <roeizavida@users.noreply.github.com>
2026-04-06 18:25:36 +0300

    feat(proto): enhance proto.plugin.zsh to manage Homebrew proto binaries and update PATH

--- custom/plugins/proto/proto.plugin.zsh
+++ custom/plugins/proto/proto.plugin.zsh
@@ -1,1 +1,15 @@
-export PATH="${HOME}/.proto/shims:${HOME}/.proto/bin:${PATH}"
+# Proto paths
+PROTO_HOME="${HOME}/.proto"
+PROTO_BIN="${PROTO_HOME}/bin"
+PROTO_SHIMS="${PROTO_HOME}/shims"
+export PATH="${PROTO_SHIMS}:${PROTO_BIN}:${PATH}"
+
+# Fix Proto binary for Homebrew
+if [[ -f "${HOMEBREW_PREFIX}/bin/proto" ]]; then
+  if [[ ! -d $PROTO_BIN ]]; then
+    mkdir -p $PROTO_BIN
+  fi
+  if [[ ! -f "${PROTO_BIN}/proto" ]]; then
+    ln -s "${HOMEBREW_PREFIX}/bin/proto" "${PROTO_BIN}/proto"
+  fi
+fi
